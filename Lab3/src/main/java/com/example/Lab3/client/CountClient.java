970a8c92f40b168d4eca9397a8da9892ea0186dc
gorazdnina <ninagorazd@gmail.com>
2021-11-09 13:43:52 +0100

    Added Count Client

---
+++ Lab3/src/main/java/com/example/Lab3/client/CountClient.java
@@ -0,0 +1,23 @@
+package com.example.Lab3.client;
+
+import com.example.Lab3.dto.CountRequest;
+import com.example.Lab3.dto.CountResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Component
+public class CountClient {
+    private RestTemplate restTemplate = new RestTemplate();
+
+    public CountResponse stringStatistics(CountRequest statisticsRequest) {
+        RequestEntity<CountRequest> request = RequestEntity
+                .post("http://localhost:8080/request%22").body(statisticsRequest);
+
+        return restTemplate.exchange(request, CountResponse.class).getBody();
+    }
+}
