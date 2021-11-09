bfadc026e2bbff1ff8e6ce447d4782b6eaf11b5c
gorazdnina <ninagorazd@gmail.com>
2021-11-09 13:42:48 +0100

    Added Request and Response

---
+++ Lab3/src/main/java/com/example/Lab3/dto/CountRequest.java
@@ -0,0 +1,14 @@
+package com.example.Lab3.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CountRequest {
+    private String string;
+    private String expression;
+    private String format;
+}
